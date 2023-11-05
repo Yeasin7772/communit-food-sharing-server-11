dcae1bd9a668b8e3b3a6b54645c6bd8805fc8e76
Yeasin7772 <yeasin26yeasin26@gmail.com>
2023-11-06 00:41:06 +0600

    get feature data in mongodb

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion,  } = require("mongodb");
 require("dotenv").config();
-const app = express();
-const port = 5000;
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
+
 
 
 
@@ -24,6 +28,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodsCollection = client.db("donationDB").collection("foods");
+
+
+
+
+    app.get("/api/v1/foods:food_quantity", async (req, res) => {
+        const food_quantity = req.params.food_quantity;
+        const query = { food_quantity: food_quantity };
+        const result = await foodsCollection.findOne(query);
+        res.send(result);
+        
+      });
+
+    app.get('/api/v1/foods',async (req,res)=> {
+        const cursor = foodsCollection.find()
+        const result = await cursor.toArray()
+        console.log(result);
+        res.send(result)
+
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
